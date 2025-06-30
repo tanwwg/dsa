55f445c3c5ecfd18eac2e88bf61345f466dc5360
Tan Thor Jen <tan@wisewizardgames.com>
2025-06-30 15:48:36 +0800

    Revert "fix quick sort"
    This reverts commit 25f6889849c35a8ab96214c8097ec601c3c0a44c.

--- dsa_practical8/quicksort.py
+++ dsa_practical8/quicksort.py
@@ -2,7 +2,7 @@ def quicksort(arr):
     if len(arr) <= 1:
         return arr
     pivot = arr[0]
-    left  = [x for x in arr[1:] if x < pivot]
+    left  = [x for x in arr[1:] if x <= pivot]
     right = [x for x in arr[1:] if x > pivot]
     return quicksort(left) + [pivot] + quicksort(right)
 
