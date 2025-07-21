c3ed5af8a2ab500caf1d2938a4ef88181b8b250e
Tan Thor Jen <tan@wisewizardgames.com>
2025-07-21 09:47:30 +0800

    fix for empty head

--- dsa_tutorial11/qn1.py
+++ dsa_tutorial11/qn1.py
@@ -6,7 +6,7 @@ class ListNode:
 head = ListNode(1, ListNode(2, ListNode(3, ListNode(4))))
 
 def findSecondLast(node, prev=None):
-    if node.next is None: return prev
+    if node is None or node.next is None: return prev
     return findSecondLast(node.next, node)
 
 print(findSecondLast(head).val)
